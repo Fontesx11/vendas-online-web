1c44b08fa148903531fd7d0b6df20e07a2eb124e
Fontesx11 <fontesdgfranca@gmail.com>
2025-05-22 08:03:59 -0300

    feat: create login page

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { BackGroundImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <>
+      <div>
+        <BackGroundImage src="./background.png" />
+      </div>
+    </>
+  );
+};
+
+export default LoginScreen;
