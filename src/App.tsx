f1395aad0935d427fff2b9d023a6fcfa8737c38f
Fontesx11 <fontesdgfranca@gmail.com>
2025-05-23 08:56:36 -0300

    feat: insert notification erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import LoginScreen from './modules/login';
+import { createBrowserRouter, type RouteObject, RouterProvider } from 'react-router-dom';
+
+import { loginRoutes } from './modules/login/routes';
+import { useNotification } from './shared/hooks/useNotifcation';
+
+const mainRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>Tela Principal</div>,
+    errorElement: <div>Página não encontrada</div>,
+  },
+];
+
+const router = createBrowserRouter([...mainRoutes, ...loginRoutes]);
 
 function App() {
-  return <LoginScreen />;
+  const { contextHolder } = useNotification();
+  return (
+    <>
+      {contextHolder}
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/shared/hooks/useResquest.ts
+++ src/shared/hooks/useResquest.ts
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import { useState } from 'react';
 
+import { useGlobalContext } from './useGlobalContext';
+
 export const useRequest = () => {
   const [loading, setLoading] = useState(false);
+  const { setNotification } = useGlobalContext();
 
   const postRequest = async (url: string, body: unknown) => {
     setLoading(true);
     const returnData = await axios
       .post(url, body)
-      .then((res) => console.log(res))
-      .catch((err) => alert(err.response.data.message));
+      .then(() => setNotification('Logado com sucesso', 'success', 'lalalala'))
+      .catch(() => setNotification('Senha Invalida', 'error', 'lalalala'));
 
     setLoading(false);
     return returnData;
