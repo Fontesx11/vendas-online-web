997d1d9df423fd0cab301b9863f1d394b925dd7c
Fontesx11 <fontesdgfranca@gmail.com>
2025-06-11 12:58:34 -0300

    style: style table

--- src/shared/types/ProductType.ts
+++ src/shared/types/ProductType.ts
@@ -1,6 +1,9 @@
+import type { CategoryType } from './CategoryType';
+
 export interface ProductType {
   id: number;
   name: string;
   price: number;
   image: string;
+  category?: CategoryType;
 }
